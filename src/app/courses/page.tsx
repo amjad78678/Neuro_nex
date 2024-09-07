85ef7ba36146eaf73ffcc7b7088aede5b5087e38
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-09-07 19:21:56 +0530

    Fix: responsive done in courses page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -10,21 +10,21 @@ const page = () => {
     setActiveTab(tab);
   };
   return (
-    <div className="p-5 md:px-10">
+    <div className="p-5 md:px-10 overflow-hidden">
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-5">
           <div className="flex flex-col gap-2">
             <h1 className="text-3xl font-bold mt-4">
               All the skills you need in one place
             </h1>
-            <h2 className="text-xl text-slate-500">
+            <h2 className="text-lg md:text-xl text-slate-500">
               From critical skills to technical topics, Udemy supports your
               professional development.
             </h2>
           </div>
         </div>
         {/* Navitems for courses */}
-        <div className="flex gap-4 items-end border-b border-slate-400">
+        <div className="flex gap-4 items-end border-b border-slate-400 overflow-x-scroll no-scrollbar">
           {[
             "Web Development",
             "It Certifications",
@@ -36,7 +36,7 @@ const page = () => {
             <h1
               key={tab}
               onClick={() => handleClick(tab)}
-              className={`font-bold cursor-pointer pb-3
+              className={`font-bold cursor-pointer pb-3 text-sm md:text-xl whitespace-nowrap
             ${
               activeTab === tab
                 ? "border-b-2 border-black text-black"
@@ -49,26 +49,26 @@ const page = () => {
         </div>
         {/* Course name big bbuttons */}
         <div className="flex gap-5 mb-4">
-          <div className="bg-black text-white px-6 py-3 rounded-full">
-            <h1 className="font-bold">Web Development</h1>
-            <p className="text-sm">13.5k learners</p>
+          <div className="bg-black text-white px-6 py-3 rounded-full ">
+            <h1 className="font-bold whitespace-nowrap">Web Development</h1>
+            <p className="text-sm whitespace-nowrap">13.5k learners</p>
           </div>
           <div className="bg-slate-200 text-black px-6 py-3 rounded-full">
-            <h1 className="font-bold">Javascript</h1>
-            <p className="text-sm">13.5k learners</p>
+            <h1 className="font-bold whitespace-nowrap">Javascript</h1>
+            <p className="text-sm whitespace-nowrap">13.5k learners</p>
           </div>
           <div className="bg-slate-200 text-black px-6 py-3 rounded-full">
-            <h1 className="font-bold">React JS</h1>
-            <p className="text-sm">13.5k learners</p>
+            <h1 className="font-bold whitespace-nowrap">React JS</h1>
+            <p className="text-sm whitespace-nowrap">13.5k learners</p>
           </div>
           <div className="bg-slate-200 text-black px-6 py-3 rounded-full">
-            <h1 className="font-bold">Angular</h1>
-            <p className="text-sm">13.5k learners</p>
+            <h1 className="font-bold whitespace-nowrap">Angular</h1>
+            <p className="text-sm whitespace-nowrap">13.5k learners</p>
           </div>
         </div>
 
         {/* Course card section */}
-        <div className="grid grid-cols-4 gap-5">
+        <div className="grid md:grid-cols-4 gap-5">
           <div className="border rounded-lg flex flex-col h-[60vh]">
             <div className="h-[50%] relative">
               <Image
